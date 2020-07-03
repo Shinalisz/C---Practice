7954997361b9eb7ed70a4fd322a92828f32926ac
Shinalisz <katalin.breszy@gmail.com>
2020-07-03 17:06:50 +0100

    commit

--- ABC.cpp
+++ ABC.cpp
@@ -1,83 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include "DEF.cpp"
+#include <chrono>
+#include <thread>
 
-#include <iostream>;
 
 using namespace std;
+
+class Player
+{
+    public:
+        const static int MAX_INPUT_CHAR_LENGTH = 100;
+        bool AnsweredAll = false;
+        char Name[MAX_INPUT_CHAR_LENGTH], 
+             Profession[MAX_INPUT_CHAR_LENGTH],
+             Age[MAX_INPUT_CHAR_LENGTH],
+             Married[MAX_INPUT_CHAR_LENGTH];
+        
+
+};
 
 void intro()
 {
     cout << endl;
     cout << "Welcome to the testing facility.\n";
     cout << "Please answer all the following questions: \n";
-}
+}    
 
-string Questions[4] = {"What is your name?", "How old are you?", "What is your profession?", "Are you married? Y/N"};
-string Answers[4] = {"Name: ", "Age: ", "Profession: ", "Married Y/N: "};
-const int MAX_NAME_LENGTH = 100;
-const int MAX_PROFESSION_LENGTH = 100;
-int Age;
-string Married;
-bool AnsweredAll = false;        
-
-char Name[MAX_NAME_LENGTH], Profession[MAX_PROFESSION_LENGTH];    
-
-void AnsweringQuestions()
+void AnsweringQuestions(Player player)
 {
+    player.AnsweredAll = false;
 
-    intro();    
+    string Questions[4] = {"What is your name?", "How old are you?", "What is your profession?", "Are you married? Y/N"};       
 
     cout << endl;
     cout << Questions[0] << endl;
-    cin.getline(Name, MAX_NAME_LENGTH);    
+    cin.getline(player.Name, player.MAX_INPUT_CHAR_LENGTH);    
     
     cout << Questions[2] << endl;
-    cin.getline(Profession, MAX_PROFESSION_LENGTH);
+    cin.getline(player.Profession, player.MAX_INPUT_CHAR_LENGTH);
 
     cout << Questions[1] << endl;
-    cin >> Age;
+    cin >> player.Age;
 
     cout << Questions[3] << endl;
-    cin >> Married;
+    cin >> player.Married;
     cout << endl;
 
-    AnsweredAll = true;
+    player.AnsweredAll = true;
 
 }
 
-int main()
-{        
-    AnsweringQuestions();
+int main(Player player)
+{
+    
+    using namespace std::chrono; //to use sleep_
+    using namespace std::this_thread; //to use sleep_
+     
+    string Answers[4] = {"Name: ", "Age: ", "Profession: ", "Married Y/N: "};    
     string DetailsCorrect;
-    while (AnsweredAll == false)
+
+    intro();
+
+    AnsweringQuestions(player);
+
+    while (player.AnsweredAll == false)
     {
         cin.clear();
         cin.ignore();
     }
 
-    if(AnsweredAll == true)
+    if(player.AnsweredAll == true)
     {
         cout << endl;
         cout << "Are these details correct?\n";
-        cout << Answers[0] << Name << endl;
-        cout << Answers[1] << Age << endl;
-        cout << Answers[2] << Profession << endl;
-        cout << Answers[3] << Married << endl;
+        cout << Answers[0] << player.Name << endl;
+        cout << Answers[1] << player.Age << endl;
+        cout << Answers[2] << player.Profession << endl;
+        cout << Answers[3] << player.Married << endl;
         cin >> DetailsCorrect;
-
-        if(DetailsCorrect == "Yes")
-        {
-            //do nothing
-        }
-        else
-        {
-            
-        }
-        
     }
+    if(DetailsCorrect == "Yes" || "yes")
+    {
+        cout << "Thank you for providing your details. We know everything about you now.\n";
+        cout << endl;
+        cout << "Processing";
+        sleep_for(1s);
+        cout << ".";
+        sleep_for(1s);
+        cout << ".";
+        sleep_for(1s);
+        cout << ".";
+        sleep_for(2s);
+        cout << endl;
+        cout << "\nYou may move onto our processing plant and begin your retraining.\n";
+        cout << endl;
+        
+        HiThere();
+        sleep_for(2s);
+        OpenFile();
+
+    }
+    else if(DetailsCorrect == "No" || "no")
+    {
+        player.AnsweredAll = false;
+        AnsweringQuestions(player);
+    }
+    else
+    {
+        cout << "Please answer with a 'Yes' or 'No'";        
+    }
+    
+   
      
     return 0;
-}
+}        
+   
 
-// Write a program to: 
-// - Ask question
-// Wait for input answer
-// once all done, print as if form and end program
+
+
+
+
