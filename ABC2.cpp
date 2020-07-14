ee7caa22a32bdb1c5ac349999bb6d60521f957bf
Shinalisz <katalin.breszy@gmail.com>
2020-07-14 17:08:03 +0100

    Y/N working

--- ABC2.cpp
+++ ABC2.cpp
@@ -4,11 +4,14 @@
 #include <thread>
 #include <Windows.h>
 #include <shellapi.h>
+#include <string>
 
 #pragma comment(lib, "SHELL32.LIB") //pragma directive - allows us to (research more!)
 
 using namespace std;
 using namespace header;
+using namespace std::chrono; //to use sleep_
+using namespace std::this_thread; //to use sleep_
 
 void intro()
 {
@@ -19,8 +22,8 @@ void intro()
 
 void Player::AnsweringQuestions()
 {        
-        AnsweredAll = false;
-    
+        
+        
         cout << endl;
         cout << Questions[0] << endl;
         cin.getline(Name, MAX_INPUT_LENGTH);    
@@ -35,7 +38,7 @@ void Player::AnsweringQuestions()
         cin.getline(Married, MAX_INPUT_LENGTH);
         cout << endl;
 
-        AnsweredAll = true;
+        
 
 }
 
@@ -58,71 +61,72 @@ void Player::OpenFile()
         
 }
 
-int main(Player)
-{            
-    using namespace std::chrono; //to use sleep_
-    using namespace std::this_thread; //to use sleep_
-    
-    Player player;
-    string DetailsCorrect;
-    intro();
-    player.AnsweringQuestions();
-    string Answers[4] = {"Name: ", "Age: ", "Profession: ", "Married Y/N: "};
+void TxtPuzzle(Player player)
+{
     int CipherSolution = 9861;
     int CipherGuess;
-    
-    while (player.AnsweredAll == false)
-    {
-        cin.clear();
-        cin.ignore();
-    }    
 
-    if(player.AnsweredAll == true)
+    cout << "Thank you for providing your details. We know everything about you now.\n";
+    cout << endl;
+    cout << "Processing";
+    sleep_for(1s);
+    cout << ".";
+    sleep_for(1s);
+    cout << ".";
+    sleep_for(1s);
+    cout << ".";
+    sleep_for(2s);
+    cout << endl;
+    cout << "\nYou may move onto our processing plant and begin your retraining.\n";
+    cout << endl;
+            
+    player.HiThere();
+    sleep_for(2s);
+    player.OpenFile();
+
+    cin >> CipherGuess;
+
+    if(CipherGuess == CipherSolution)
     {
+        cout << "Your brainfunctions are adequate. You may proceed.";
+    }        
+}
+
+int main(Player)
+{            
+        
+    Player player;
+    
+    intro();     
+
+    string Answers[4] = {"Name: ", "Age: ", "Profession: ", "Married Y/N: "};        
+     
+     
+     
+    char DetailesCorrect[25];    
+
+    do
+    {
+        player.AnsweringQuestions();  
+
         cout << endl;
-        cout << "Are these details correct?\n";
+        cout << "Are these details correct? Yes/No\n";
         cout << Answers[0] << player.Name << endl;
         cout << Answers[1] << player.Age << endl;
         cout << Answers[2] << player.Profession << endl;
-        cout << Answers[3] << player.Married << endl;
-        cin >> DetailsCorrect;
-
-        if(DetailsCorrect == "Yes")
-        {
-            cout << "Thank you for providing your details. We know everything about you now.\n";
-            cout << endl;
-            cout << "Processing";
-            sleep_for(1s);
-            cout << ".";
-            sleep_for(1s);
-            cout << ".";
-            sleep_for(1s);
-            cout << ".";
-            sleep_for(2s);
-            cout << endl;
-            cout << "\nYou may move onto our processing plant and begin your retraining.\n";
-            cout << endl;
+        cout << Answers[3] << player.Married << endl;      
             
-            player.HiThere();
-            sleep_for(2s);
-            player.OpenFile();
-
-            cin >> CipherGuess;
-
-            if(CipherGuess == CipherSolution)
-            {
-                cout << "Your brainfunctions are adequate. You may proceed.";
-            }
-            
-        }
-        else
-        {
-            /*code*/
-        }
+        cin.getline(DetailesCorrect, 25);  
         
-    }
+        if (DetailesCorrect == std::string("Yes"))
+        {    
+            break;
+        }
 
+    } while (DetailesCorrect == std::string("No"));
     
+    TxtPuzzle(player);
+        
      
     return 0;
 }
